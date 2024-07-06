a14abce075436c02bb8d752b96de4c6e652d3c87
Rogier van Staveren <rogier@batoid.com>
2024-07-06 07:59:54 +0200

    Remove obsolete manual path

--- custom_components/benqprojector/const.py
+++ custom_components/benqprojector/const.py
@@ -8,7 +8,6 @@ CONF_TYPE_SERIAL: Final = "serial"
 CONF_TYPE_TELNET: Final = "telnet"
 
 CONF_SERIAL_PORT: Final = "serial_port"
-CONF_MANUAL_PATH: Final = "manual_path"
 CONF_PROJECTOR: Final = "projector"
 CONF_BAUD_RATE: Final = "baud_rate"
 CONF_INTERVAL: Final = "interval"
