6813c569d0fcf39638de00bde71f0c12efb41928
Rogier van Staveren <rogier@batoid.com>
2024-07-06 08:01:53 +0200

    Remove obsolete CONF_PROJECTOR

--- custom_components/benqprojector/const.py
+++ custom_components/benqprojector/const.py
@@ -8,7 +8,6 @@ CONF_TYPE_SERIAL: Final = "serial"
 CONF_TYPE_TELNET: Final = "telnet"
 
 CONF_SERIAL_PORT: Final = "serial_port"
-CONF_PROJECTOR: Final = "projector"
 CONF_BAUD_RATE: Final = "baud_rate"
 CONF_INTERVAL: Final = "interval"
 CONF_DEFAULT_INTERVAL: Final = 5
